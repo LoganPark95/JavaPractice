a749f3ed016c7e7ceff4d31b3efef3676289a689
Logan Park <logan.js.park@gmail.com>
2018-12-21 19:29:39 -0500

    Added a method

--- AP_CompSci/src/sumColumn.java
+++ AP_CompSci/src/sumColumn.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 public class sumColumn {
+	public static double sumColumn(double[][] m, int columnIndex) {
+		double sum = 0;
+		for(int i = 0; i < 3 ; i++) {
+			sum += m[i][columnIndex];
+		}
+		return sum;
+	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int counter = 0;
-		double[] sum = new double[4];
 		System.out.println("Enter a 3 by 4 matrix row by row: ");
 		double[][] myArray = new double[3][4];
 		for(int i = 0; i < 3; i++) {
@@ -11,11 +17,7 @@ public class sumColumn {
 				myArray[i][j] = input.nextDouble();
 			}
 		}
-		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 3; j++) {
-				sum[i] += myArray[j][i];
-			}
-		}
+		
 		System.out.println("This is my Array: ");
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 4; j++) {
@@ -29,7 +31,7 @@ public class sumColumn {
 		}
 		System.out.println("\n" + "This is the Sum of the Arrays: ");
 		for(int i = 0; i < 4; i++) {
-			System.out.print(sum[i] + " ");
+			System.out.print(sumColumn(myArray, i) + " ");
 		}
 		System.out.println();
 	}
