6614b7b608725aa2b95a12a319847317a23f25cd
Logan Park <logan.js.park@gmail.com>
2018-12-21 19:14:15 -0500

    finished problem solving

--- AP_CompSci/src/sumColumn.java
+++ AP_CompSci/src/sumColumn.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 public class sumColumn {
 	public static void main(String[] args) {
-		Scanner Andrea = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		int counter = 0;
+		double[] sum = new double[4];
 		System.out.println("Enter a 3 by 4 matrix row by row: ");
 		double[][] myArray = new double[3][4];
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 4; j++) {
-				myArray[i][j] = Andrea.nextDouble();
+				myArray[i][j] = input.nextDouble();
 			}
 		}
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 3; j++) {
+				sum[i] += myArray[j][i];
+			}
+		}
+		System.out.println("This is my Array: ");
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 4; j++) {
 				System.out.print(myArray[i][j] + " ");
@@ -20,5 +27,10 @@ public class sumColumn {
 				}
 			}
 		}
+		System.out.println("\n" + "This is the Sum of the Arrays: ");
+		for(int i = 0; i < 4; i++) {
+			System.out.print(sum[i] + " ");
+		}
+		System.out.println();
 	}
 }
